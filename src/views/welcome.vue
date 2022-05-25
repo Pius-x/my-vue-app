060fb553b6b21f6298595c36aea594b3e8f7668c
Pius-x <1105317070@qq.com>
2022-05-25 15:09:52 +0800

    perf: 【首页设计】

--- src/views/welcome.vue
+++ src/views/welcome.vue
@@ -1,3 +1,12 @@
+<script setup lang="ts">
+import illustration2 from "/@/assets/home/home.svg?component";
+</script>
+
 <template>
-  <h1>VUE-ADMIN</h1>
+  <el-card>
+    <div style="text-align: center">
+      <h2 style="font-size: 25px; margin-bottom: 20px"><strong>欢迎登录</strong></h2>
+      <component :is="illustration2" style="display: block; margin: auto" />
+    </div>
+  </el-card>
 </template>
